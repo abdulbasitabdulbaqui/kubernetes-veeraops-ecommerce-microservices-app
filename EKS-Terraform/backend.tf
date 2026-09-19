aa221c3176875bb14f0bd7e13a6c103c00b5a52f
Ansari Abdul Basit <abdulbasitabdulbaqui@gmail.com>
2026-09-19 21:53:05 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "ansari9168"
+    bucket       = "ansari916818"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
